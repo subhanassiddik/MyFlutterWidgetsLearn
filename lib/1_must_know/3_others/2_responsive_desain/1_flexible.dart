9071ca8bbc5fe40ce7a5849a897a928f15ea1a56
subhanassiddik <subhanassiddik@gmail.com>
2021-11-18 17:39:22 +0800

    responsive : flexible widget

---
+++ lib/1_must_know/3_others/2_responsive_desain/1_flexible.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class WidgetFlexible extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Flexible(
+            flex: 1,
+            child: Row(
+              children: [
+                mycontainer(warna: Colors.black, flex: 1),
+                mycontainer(warna: Colors.green, flex: 1),
+                mycontainer(warna: Colors.purple, flex: 1),
+              ],
+            ),
+          ),
+          mycontainer(warna: Colors.red, flex: 2),
+          mycontainer(warna: Colors.yellow, flex: 1),
+        ],
+      ),
+    );
+  }
+}
+
+Flexible mycontainer({Color warna, int flex}) {
+  return Flexible(flex: flex, child: Container(color: warna));
+}
